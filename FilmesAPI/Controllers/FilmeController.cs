94eb0422a78c5215da47c1f0b7e6e8ed9f359cb2
andre.bronca <bronca.andre@gmail.com>
2025-04-03 18:05:37 -0300

    Criação de model. Criação de Controller. Teste com PostMan / Swagger

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+	private static List<Filme> filmes = new();
+
+	[HttpPost]
+	public void AdicionaFilme([FromBody] Filme filme)
+	{
+		filmes.Add(filme);
+		ExibeFilmes();
+	}
+
+	private void ExibeFilmes()
+	{
+		foreach (var item in filmes)
+		{
+			Console.WriteLine(item.Titulo + ", " + item.Genero + ", " + item.Duracao);
+		}
+	}
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,13 @@
+﻿namespace FilmesAPI.Models;
+
+public class Filme
+{
+	public string Titulo { get; set; }
+	public string Genero { get; set; }
+	public int Duracao { get; set; }
+
+	public override string ToString()
+	{
+		return @"Título: {Titulo}, Gênero: {Genero}, Duração: {Duracao}";
+	}
+}
